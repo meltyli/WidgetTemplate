e41ca44a94e18e3eb8bcda8e6af4c262db24b442
meltyli <n99273792qut.edu.au>
2024-05-06 11:22:15 +1000

    updated namespaces to reflect project's name

--- WidgetTemplate/App.xaml.cs
+++ WidgetTemplate/App.xaml.cs
@@ -16,7 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Gaming.XboxGameBar;
 
-namespace CopyingFromSample
+namespace WidgetTemplate
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
